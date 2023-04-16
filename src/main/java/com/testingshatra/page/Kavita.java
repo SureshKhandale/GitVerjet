6a2fd75f1e13b1c9b67a7d52fa06a1a1494c4bf6
Awaghan <sumeet.awaghan@capgemini.com>
2023-04-16 19:44:17 +0530

    this is my first commit

---
+++ src/main/java/com/testingshatra/page/Kavita.java
@@ -0,0 +1,11 @@
+package com.testingshatra.page;
+
+public class Kavita {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello");
+		System.out.println("Testing");
+	}
+
+}
