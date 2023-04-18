9d31554cbe4cb12031cc314d542aeb8526b174d3
sonalitchaudhari <sonalitchaudhari@gmail.com>
2023-04-18 11:07:20 +0530

    my commit

--- src/test/java/com/signup/Sonali.java
+++ src/test/java/com/signup/Sonali.java
@@ -3,9 +3,11 @@ package com.signup;
 public class Sonali {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		System.out.println("hello project");
-		System.out.println("Verjet")
+		System.out.println("Verjet");
+		System.out.println("hi");
+		System.out.println("test");
+		
 	}
 
 }
